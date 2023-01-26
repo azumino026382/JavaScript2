7aa11953d5eb33132ec58a6636ac3c2227a6af0f
EC2 Default User <ec2-user@ip-172-31-47-108.ap-northeast-1.compute.internal>
2023-01-26 16:16:42 +0000

    電卓提出2回目

--- main.js
+++ main.js
@@ -7,6 +7,16 @@ function calculate(target) {
         result.innerHTML = "0";
     } else if (targetValue == "=") {
         result.innerHTML = eval(result.innerHTML);
+    } else if (targetValue == "+") {
+        if (result.innerHTML.slice(-1) == "+") {
+            $(document).ready(function(){
+             $(".buttonOperator").onclick(function() {
+              $(".result").text(targetValue);
+             });
+            });    
+        } else {    
+            result.innerHTML += targetValue;
+        }
     } else {
         if (result.innerHTML == "0") {
             result.innerHTML = targetValue;
@@ -17,3 +27,4 @@ function calculate(target) {
         
     }
 }
+
