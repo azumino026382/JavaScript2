55f06f06d5723489fb22e91a968e5a7828c7acac
EC2 Default User <ec2-user@ip-172-31-47-108.ap-northeast-1.compute.internal>
2023-01-31 14:15:24 +0000

    電卓提出3回目

--- main.js
+++ main.js
@@ -5,15 +5,74 @@ function calculate(target) {
 
     if (targetValue == "AC") {
         result.innerHTML = "0";
+    } else if (targetValue == "00")
+        if (result.innerHTML == "0") {
+            result.innerHTML = "0";
+        } else {
+            result.innerHTML += targetValue;
     } else if (targetValue == "=") {
         result.innerHTML = eval(result.innerHTML);
     } else if (targetValue == "+") {
         if (result.innerHTML.slice(-1) == "+") {
-            $(document).ready(function(){
-             $(".buttonOperator").onclick(function() {
-              $(".result").text(targetValue);
-             });
-            });    
+            let slice = result.innerHTML.slice(0, -1);
+            result.innerHTML = slice + targetValue;
+        } else if (result.innerHTML.slice(-1) == "-") {
+            let slice = result.innerHTML.slice(0, -1);
+            result.innerHTML = slice + targetValue;
+        } else if (result.innerHTML.slice(-1) == "*") {
+            let slice = result.innerHTML.slice(0, -1);
+            result.innerHTML = slice + targetValue;
+        } else if (result.innerHTML.slice(-1) == "/") {
+            let slice = result.innerHTML.slice(0, -1);
+            result.innerHTML = slice + targetValue;
+        } else {    
+            result.innerHTML += targetValue;
+        }
+    } else if (targetValue == "-") {
+        if (result.innerHTML.slice(-1) == "+") {
+            let slice = result.innerHTML.slice(0, -1);
+            result.innerHTML = slice + targetValue;
+        } else if (result.innerHTML.slice(-1) == "-") {
+            let slice = result.innerHTML.slice(0, -1);
+            result.innerHTML = slice + targetValue;
+        } else if (result.innerHTML.slice(-1) == "*") {
+            let slice = result.innerHTML.slice(0, -1);
+            result.innerHTML = slice + targetValue;
+        } else if (result.innerHTML.slice(-1) == "/") {
+            let slice = result.innerHTML.slice(0, -1);
+            result.innerHTML = slice + targetValue;
+        } else {    
+            result.innerHTML += targetValue;
+        }
+    } else if (targetValue == "*") {
+        if (result.innerHTML.slice(-1) == "+") {
+            let slice = result.innerHTML.slice(0, -1);
+            result.innerHTML = slice + targetValue;
+        } else if (result.innerHTML.slice(-1) == "-") {
+            let slice = result.innerHTML.slice(0, -1);
+            result.innerHTML = slice + targetValue;
+        } else if (result.innerHTML.slice(-1) == "*") {
+            let slice = result.innerHTML.slice(0, -1);
+            result.innerHTML = slice + targetValue;
+        } else if (result.innerHTML.slice(-1) == "/") {
+            let slice = result.innerHTML.slice(0, -1);
+            result.innerHTML = slice + targetValue;
+        } else {    
+            result.innerHTML += targetValue;
+        }
+    } else if (targetValue == "/") {
+        if (result.innerHTML.slice(-1) == "+") {
+            let slice = result.innerHTML.slice(0, -1);
+            result.innerHTML = slice + targetValue;
+        } else if (result.innerHTML.slice(-1) == "-") {
+            let slice = result.innerHTML.slice(0, -1);
+            result.innerHTML = slice + targetValue;
+        } else if (result.innerHTML.slice(-1) == "*") {
+            let slice = result.innerHTML.slice(0, -1);
+            result.innerHTML = slice + targetValue;
+        } else if (result.innerHTML.slice(-1) == "/") {
+            let slice = result.innerHTML.slice(0, -1);
+            result.innerHTML = slice + targetValue;
         } else {    
             result.innerHTML += targetValue;
         }
